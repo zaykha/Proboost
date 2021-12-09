c9e28c4add951af33cf97be5e74cf228bfd7f804
zaykha <zaykha@gmail.com>
2021-12-09 15:25:08 +0630

    initial installation router, styled component, scroll

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+       <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo>
+                    ProBoost
+                </NavLogo>
+            </NavbarContainer>
+        </Nav>
+       </>
+    )
+}
